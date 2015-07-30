75a52d3914c0ad33b188908bd653cd511e3fe37f
Ho-kyeong <ghrud0520@naver.com>
2015-07-30 22:34:43 +0900

    Update and rename LetterCapitalize_incorrect.rb to LetterCapitalize.rb

--- LetterCapitalize.rb
+++ LetterCapitalize.rb
@@ -3,13 +3,13 @@ def LetterCapitalize(str)
   # code goes here
 
   a=str.split(' ')[0].capitalize
+  for i in (1..str.split(' ').length-1)
   a<<" "
-  a<<str.split(' ')[1].capitalize
-  a<<" "
-  a<<str.split(' ')[2].capitalize
+  a<<str.split(' ')[i].capitalize
+  end
   return a      
 end
    
 # keep this function call here 
 # to see how to enter arguments in Ruby scroll down   
-LetterCapitalize(STDIN.gets)  
+LetterCapitalize(STDIN.gets) 
