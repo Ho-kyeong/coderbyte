a328d815b7e6f0c6a78d663630537efcf65fa947
Ho-kyeong <ghrud0520@naver.com>
2015-07-31 20:50:28 +0900

    Create ABCheck.rb

---
+++ ABCheck/ABCheck.rb
@@ -0,0 +1,18 @@
+def ABCheck(str)
+
+  # code goes here
+  str = str.split('')
+  for i in (0 .. str.split('').length)
+    if str[i] == 'a' && str[i+4] == 'b'
+      return true
+    elsif str[i] == 'b' && str[i+4] == 'a'
+      return true
+    else
+      return false
+    end
+  end
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+ABCheck(STDIN.gets) 
